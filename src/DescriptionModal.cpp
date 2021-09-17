8dd1e0fe993d08840c5d7f03591e7110e05c5e13
hrvojekolar <hrvojekolar1998@gmail.com>
2021-09-17 09:49:12 +0200

    add DescriptionModal for GameInfoPopUp

---
+++ src/DescriptionModal.cpp
@@ -0,0 +1,54 @@
+#include <Input/Input.h>
+#include <Pins.hpp>
+#include "DescriptionModal.h"
+#include <ByteBoi.h>
+
+DescriptionModal* DescriptionModal::instance = nullptr;
+
+DescriptionModal::DescriptionModal(Context& context, const char* title, const char* description) : Modal(context, 80, 80),
+																		 layout(new LinearLayout(&screen, VERTICAL)),
+																		 descriptionText(new DescriptionModalItem(layout,title,description))
+																		 {
+	instance = this;
+	buildUI();
+}
+
+DescriptionModal::~DescriptionModal(){
+
+}
+
+void DescriptionModal::draw(){
+	screen.getSprite()->clear(C_HEX(0x53565c));
+	screen.getSprite()->drawRect(screen.getTotalX(),screen.getTotalY(),80,80,C_HEX(0x9197a1));
+	screen.draw();
+}
+
+void DescriptionModal::start(){
+	Input::getInstance()->setBtnPressCallback(BTN_B, [](){
+		if(instance == nullptr) return;
+		instance->pop();
+	});
+	draw();
+	screen.commit();
+}
+
+void DescriptionModal::stop(){
+	Input::getInstance()->removeBtnPressCallback(BTN_B);
+}
+
+void DescriptionModal::init(){
+	Context::init();
+
+}
+
+void DescriptionModal::deinit(){
+	Context::deinit();
+}
+
+void DescriptionModal::buildUI(){
+	layout->setWHType(CHILDREN, CHILDREN);
+	layout->addChild(descriptionText);
+	layout->reflow();
+	screen.addChild(layout);
+	screen.repos();
+}
---
+++ src/DescriptionModal.h
@@ -0,0 +1,36 @@
+#ifndef BYTEBOI_FIRMWARE_DESCRIPTIONMODAL_H
+#define BYTEBOI_FIRMWARE_DESCRIPTIONMODAL_H
+
+#include <Support/Modal.h>
+#include <Loop/LoopListener.h>
+#include <UI/LinearLayout.h>
+#include <Elements/ModalBackground.hpp>
+#include "DescriptionModalItem.h"
+
+class DescriptionModal : public Modal{
+public:
+	DescriptionModal(Context& context, const char* title, const char* description);
+
+	~DescriptionModal();
+
+	void draw() override;
+
+	void start() override;
+
+	void stop() override;
+
+protected:
+	void init() override;
+
+	void deinit() override;
+
+private:
+	static DescriptionModal* instance;
+
+	void buildUI();
+
+	LinearLayout* layout;
+	DescriptionModalItem* descriptionText;
+};
+
+#endif //BYTEBOI_FIRMWARE_DESCRIPTIONMODAL_H
