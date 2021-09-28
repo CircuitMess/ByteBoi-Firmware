d79d77693dda62e825a8dd34ecdc128d5b0cd29c
hrvojekolar <hrvojekolar1998@gmail.com>
2021-09-28 12:05:57 +0200

    Add SettingsScreen

---
+++ src/Settings/SettingsScreen.cpp
@@ -0,0 +1,2 @@
+#include "SettingsScreen.h"
+
---
+++ src/Settings/SettingsScreen.h
@@ -0,0 +1,52 @@
+#ifndef BYTEBOI_FIRMWARE_SETTINGSSCREEN_H
+#define BYTEBOI_FIRMWARE_SETTINGSSCREEN_H
+
+#include <Support/Context.h>
+#include <UI/LinearLayout.h>
+#include "SliderElement.h"
+#include "DiscreteSlider.h"
+#include <Input/InputListener.h>
+#include "TextElement.h"
+#include <FS.h>
+
+namespace SettingsScreen {
+	class SettingsScreen : public Context, private InputListener {
+	public:
+
+		SettingsScreen(Display &display);
+
+		virtual ~SettingsScreen();
+
+		void start();
+
+		void stop();
+
+		void draw();
+
+	protected:
+		void deinit() override;
+
+		void init() override;
+
+
+	private:
+		static SettingsScreen *instance;
+
+		LinearLayout* screenLayout;
+		DiscreteSlider* shutDownSlider;
+		TextElement* inputTest;
+		TextElement* save;
+
+		int selectedSetting = 0;
+		bool disableMainSelector = false;
+
+		void buildUI();
+		void buttonPressed(uint id) override;
+		void selectApp(int8_t num);
+
+		Color* backgroundBuffer= nullptr;
+		fs::File introSong;
+	};
+}
+
+#endif //BYTEBOI_FIRMWARE_SETTINGSSCREEN_H
