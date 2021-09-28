54b4963dd209ef6c33e94bab927681dcdcc18d7e
hrvojekolar <hrvojekolar1998@gmail.com>
2021-09-28 16:40:29 +0200

    Alling text in SettingsElement

--- src/Settings/SettingsElement.cpp
+++ src/Settings/SettingsElement.cpp
@@ -1,6 +1,6 @@
 #include "SettingsElement.h"
 
-SettingsScreen::SettingsElement::SettingsElement(ElementContainer* parent, String name) : CustomElement(parent,160,20), name(name){
+SettingsScreen::SettingsElement::SettingsElement(ElementContainer* parent, String name) : CustomElement(parent,160,17), name(name){
 
 }
 
@@ -13,10 +13,10 @@ void SettingsScreen::SettingsElement::draw(){
 	getSprite()->setTextColor(TFT_WHITE);
 	getSprite()->setTextSize(1);
 	getSprite()->setTextFont(1);
-	getSprite()->setCursor(getTotalX() + 5, getTotalY() + 10);
+	getSprite()->setCursor(getTotalX() + 5, getTotalY() + 7);
 	getSprite()->println(name);
 	if(selected){
-		getSprite()->drawRect(getTotalX() + 3, getTotalY() + 6, 86, 15, TFT_WHITE);
+		getSprite()->drawRect(getTotalX() + 3, getTotalY() + 3, 86, 15, TFT_WHITE);
 	}
 	drawControl();
 }
