9a9eb65417dfa310d9401cf4e44cd135547f88b2
hrvojekolar <hrvojekolar1998@gmail.com>
2021-10-19 13:09:22 +0200

    Add SPIFFS files size in HWTestChecksums

--- src/JigHWTest/HWTestChecksums.hpp
+++ src/JigHWTest/HWTestChecksums.hpp
@@ -4,43 +4,17 @@ static const struct {
 	String name;
 	uint32_t sum;
 } SPIFFSChecksums[] = {
-		{"/Simple/actionBorder.raw",23592 },
-		{"/Simple/add.raw",42174 },
-		{"/Simple/add_button.raw",42862 },
-		{"/Simple/app_bg.raw.hs", 91530},
-		{"/Simple/arrow_down.raw", 100450},
-		{"/Simple/arrow_left.raw", 136234},
-		{"/Simple/arrow_right.raw", 109200},
-		{"/Simple/arrow_up.raw", 149674},
-		{"/Simple/border15x15.raw", 17745},
-		{"/Simple/delete_button.raw", 55862},
-		{"/Simple/light_off.raw", 98031},
-		{"/Simple/light_on.raw", 117359},
-		{"/Simple/pause.raw", 82592},
-		{"/Simple/playback_bg.raw.hs",86813},
-		{"/Simple/simple_edit_bg.raw.hs", 97564},
+		{"/launcher/stock/emptySD.raw",8192 },
+		{"/launcher/stock/load.raw",8192 },
+		{"/launcher/stock/noIcon.raw",8192 },
+		{"/launcher/stock/NoSD.raw",8192 },
+		{"/launcher/stock/Setts.raw",8192 },
 
-		{"/Setts/settings_bg.raw.hs", 91334},
+		{"/launcher/Setts/SettsBg.raw.hs",473 },
 
-		{"/Intro/intro.g565.hs", 4235833},
+		{"/launcher/Logo/BBLogo.raw",4836 },
 
-		{"/MainMenu/app_autonomous.raw", 607697},
-		{"/MainMenu/app_ball.raw", 671122},
-		{"/MainMenu/app_object.raw", 475486},
-		{"/MainMenu/app_settings.raw", 551752},
-		{"/MainMenu/app_simple.raw", 536776},
-		{"/MainMenu/mainmenu_bg.raw.hs", 91530},
-		{"/MainMenu/mainmenu_border.raw", 116440},
+		{"/launcher/intro/intro.g565.hs",5666 },
 
-		{"/AutoDrive/engine.bw", 5765},
-		{"/AutoDrive/percentage.raw", 7866},
-		{"/AutoDrive/raw_bg.raw.hs", 6475},
-
-		{"/battery_0.raw", 25668},
-		{"/battery_1.raw", 27388},
-		{"/battery_2.raw", 29172},
-		{"/battery_3.raw", 33300},
-		{"/battery_4.raw", 34052},
-		{"/chargingIcon.raw", 6531}
 
 };
