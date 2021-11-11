dd487b58b91941768c9afd03bb7241177d946938
Filip Budiša <filip@circuitmess.com>
2021-11-11 14:17:47 +0100

    Add SD checks in SDcardHWTest

--- src/UserHWTest/SDcardHWTest.h
+++ src/UserHWTest/SDcardHWTest.h
@@ -8,7 +8,7 @@
 #include <UI/LinearLayout.h>
 #include "HWTestPart.h"
 
-class SDcardHWTest : public HWTestPart{
+class SDcardHWTest : public HWTestPart, public LoopListener  {
 public:
 	SDcardHWTest(UserHWTest* userHwTest);
 
@@ -21,10 +21,12 @@ public:
 	void stop() override;
 
 	void buttonPressed(uint id) override;
+	void loop(uint micros) override;
 private:
 	uint8_t doneCounter = 0;
 	bool soundIsDone = false;
 	std::vector<String> item;
+	bool inserted = false;
 };
 
 #endif //BYTEBOI_FIRMWARE_SDCARDHWTEST_H
